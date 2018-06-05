4d20c3bea6c72e971ce44b6a0c964d332e656cda
swacad <swacad@users.noreply.github.com>
2018-06-04 20:34:20 -0400

    Complete 2_struct_elementary exercise

--- 4_basic_assembly/3_memory/6_structures/ex/1_read_code/2_struct_elementary.asm
+++ 4_basic_assembly/3_memory/6_structures/ex/1_read_code/2_struct_elementary.asm
@@ -21,52 +21,52 @@ entry start
 include 'win32a.inc' 
 
 struct PNT
-    x   db  ?
-    y   db  ?
-ends
+    x   db  ?                   ; 0
+    y   db  ?                   ; 1
+ends                            ; 2
 
 struct LINE
-    p_start     PNT     ?
-    p_end       PNT     ?
-ends
+    p_start     PNT     ?       ; 0
+    p_end       PNT     ?       ; 2
+ends                            ; 4
 
 struct COLORED_LINE
     struct
-        red     db      ?
-        green   db      ?
-        blue    db      ?
-                db      ?
-    ends
-    cline   LINE    ?
-ends
+        red     db      ?       ; 0
+        green   db      ?       ; 1
+        blue    db      ?       ; 2
+                db      ?       ; 3
+    ends                        
+    cline   LINE    ?           ; 4
+ends                            ; 8
 
 struct UNI_COLORED_LINE
     union
         struct
-            red     db      ?
-            green   db      ?
-            blue    db      ?
-                    db      ?
+            red     db      ?   ; 0
+            green   db      ?   ; 1
+            blue    db      ?   ; 2
+                    db      ?   ; 3
         ends
-        color   dd      ?
+        color   dd      ?       ; 0
     ends
-    cline   LINE    ?
-ends
+    cline   LINE    ?           ; 4
+ends                            ; 8
 
 
 struct INDEXED_LINE
-    index   dd  ?
+    index   dd  ?               ; 0
     union
         struct
-            red     db      ?
-            green   db      ?
-            blue    db      ?
-                    db      ?
+            red     db      ?   ; 4
+            green   db      ?   ; 5
+            blue    db      ?   ; 6
+                    db      ?   ; 7
         ends
-        color   dd      ?
+        color   dd      ?       ; 4
     ends
-    cline   LINE    ?
-ends
+    cline   LINE    ?           ; 8
+ends                            ; 12
 
 ; ===============================================
 section '.bss' readable writable
@@ -85,27 +85,27 @@ start:
 
     ; Compare the following two values:
     mov     eax,UNI_COLORED_LINE.red
-    call    print_eax
+    call    print_eax   ; 0
 
     mov     eax,UNI_COLORED_LINE.color
-    call    print_eax
+    call    print_eax   ; 0
 
     ; And also compare the following two values:
     mov     eax,my_u_line.red
-    call    print_eax
+    call    print_eax   ; my_u_line + 0
 
     mov     eax,my_u_line.color
-    call    print_eax
+    call    print_eax   ; my_u_line + 0
 
 
     mov     dword [my_u_line.color],11223344h
     ; Predict the following values:
     movzx   eax,byte [my_u_line.red]
-    call    print_eax
+    call    print_eax   ; 44
     movzx   eax,byte [my_u_line.green]
-    call    print_eax
+    call    print_eax   ; 33
     movzx   eax,byte [my_u_line.blue]
-    call    print_eax
+    call    print_eax   ; 22
 
     ; Print a delimiter.
     call    print_delimiter
@@ -116,13 +116,13 @@ start:
 
     ; Predict the following values:
     mov     eax,INDEXED_LINE.red
-    call    print_eax
+    call    print_eax   ; 4
 
     mov     eax,sizeof.INDEXED_LINE
-    call    print_eax
+    call    print_eax   ; 0xC
 
     mov     eax,INDEXED_LINE.cline.p_start.y
-    call    print_eax
+    call    print_eax   ; 9
 
 
     ; Exit the process:
