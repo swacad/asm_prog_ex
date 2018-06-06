dd1b5a772e21ba296d79c57bc1456d45d7b5b8f8
swacad <swacad@users.noreply.github.com>
2018-06-06 19:18:23 -0400

    Fix bug in lexicographic sort

--- 4_basic_assembly/3_memory/6_structures/ex/2_write_code/2_lexicographic_sort.asm
+++ 4_basic_assembly/3_memory/6_structures/ex/2_write_code/2_lexicographic_sort.asm
@@ -79,6 +79,9 @@ start:
     call print_delimiter
 
 first_pass:     ; Compare p1 and p2
+    mov eax, 10h
+    call print_eax
+    
     movzx eax, byte [p1.x]
     cmp al, byte [p2.x]     ; compare p1.x to p2.x
     jg p1_greater_p2
@@ -86,12 +89,18 @@ first_pass:     ; Compare p1 and p2
     jmp second_pass
 
 first_pass_check_second:
+    mov eax, 11h
+    call print_eax
+
     movzx eax, byte [p1.y]
     cmp al, byte [p2.y]     ; compare p1.y to p2.y
     jg p1_greater_p2
     jmp second_pass
 
 p1_greater_p2:
+    mov eax, 12h
+    call print_eax
+    
     mov [s2], p1
     mov [s1], p2
     
@@ -104,24 +113,27 @@ p1_greater_p2:
     call print_delimiter
 
 second_pass:    ; compare [s1] and p3
+    mov eax, 20h
+    call print_eax
     mov esi, [s1]
     movzx eax, byte [esi]
     cmp al, byte [p3.x]
     jg s1_greater_p3    ; point in s1 is greater than p3
     je second_pass_check_second
     jmp third_pass
-    mov eax, 11h
-    call print_eax
 
 second_pass_check_second:
+    mov eax, 21h
+    call print_eax
     movzx eax, byte [esi + 1]
     cmp al, byte [p3.y]
     jg s1_greater_p3    
     jmp third_pass
     
 s1_greater_p3:
-    mov eax, 21h
+    mov eax, 22h
     call print_eax
+    
     mov eax, [s1]   ; Store previous lowest point in EAX
     mov ebx, [s2]   ; store current middle point in EBX
     
@@ -138,22 +150,41 @@ s1_greater_p3:
     call print_eax
     call print_delimiter
 
-third_pass: ; compare p2 and p3
-    movzx eax, byte [p2.x]
-    cmp al, byte [p3.x]     ; compare p2.x to p3.x
-    jg p2_greater_p3
+third_pass: ; compare [s2] and [s3]
+    mov eax, 30h
+    call print_eax
+    
+    mov esi, [s2]
+    movzx eax, byte [esi]
+    mov esi, [s3]
+    movzx ebx, byte [esi]
+    cmp eax, ebx
+    jg s2_greater_s3    ; point in s2 is greater than point in s3
     je third_pass_check_second
     jmp exit
 
 third_pass_check_second:
-    movzx eax, byte [p2.y]
-    cmp al, byte [p3.y]     ; compare p1.y to p2.y
-    jg p2_greater_p3
+    mov eax, 31h
+    call print_eax
+    
+    mov esi, [s2]
+    movzx eax, byte [esi + 1]
+    
+    mov esi, [s3]
+    movzx ebx, byte [esi + 1]
+    
+    cmp eax, ebx    ; compare [s2].y to [s3].y
+    jg s2_greater_s3
     jmp exit
-
-p2_greater_p3:
-    mov [s3], p2
-    mov [s2], p3
+    
+s2_greater_s3:
+    mov eax, 32h
+    call print_eax
+    
+    mov ebx, [s2]
+    mov ecx, [s3]
+    mov [s2], ecx
+    mov [s3], ebx
     
     mov eax, [s1]
     call print_eax
@@ -162,11 +193,31 @@ p2_greater_p3:
     mov eax, [s3]
     call print_eax
     call print_delimiter
+    
 
 exit:
+    ; print coordinates in order
+    mov esi, [s1]
+    movzx eax, byte [esi]
+    call print_eax
+    movzx eax, byte [esi + 1]
+    call print_eax
+    
+    mov esi, [s2]
+    movzx eax, byte [esi]
+    call print_eax
+    movzx eax, byte [esi + 1]
+    call print_eax
+    
+    mov esi, [s3]
+    movzx eax, byte [esi]
+    call print_eax
+    movzx eax, byte [esi + 1]
+    call print_eax
+    
     ; Exit the program:
-        push    0
-        call    [ExitProcess]
+    push    0
+    call    [ExitProcess]
 
 
 include 'training.inc'
