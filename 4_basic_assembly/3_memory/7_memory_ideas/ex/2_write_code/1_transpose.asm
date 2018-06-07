17dfaa33093ae606c182a360ed6a607ddcf01e16
swacad <swacad@users.noreply.github.com>
2018-06-07 19:12:20 -0400

    Fix transpose

--- 4_basic_assembly/3_memory/7_memory_ideas/ex/2_write_code/1_transpose.asm
+++ 4_basic_assembly/3_memory/7_memory_ideas/ex/2_write_code/1_transpose.asm
@@ -38,13 +38,18 @@ entry start
 
 include 'win32a.inc' 
 
-WIDTH = 4h
+WIDTH = 3h
 HEIGHT = 2h
+
+section '.data' data readable writeable
+    four db 4
+
 ; ===============================================
 section '.bss' readable writeable
     ; Declare the uninitialized table in memory:
     A     dd      WIDTH*HEIGHT dup (?)
     B     dd      WIDTH*HEIGHT dup (?)
+    
 
 ; ===============================================
 section '.text' code readable executable
@@ -56,6 +61,7 @@ start:
 
 next_row:
     mov     ebx,0   ; Column counter.
+    
 next_column:
     mov     eax,ecx
     
@@ -72,32 +78,57 @@ next_column:
     inc     ecx
     cmp     ecx,HEIGHT
     jnz     next_row    ; Loop until all rows completed
-
-; Transpose A into B
-transpose:
-    mov     esi,A ; Cell ptr.
-    mov edi, B
-    mov     ebx,0   ; column counter
-
-transpose_next_column:
-    mov     ecx,0   ; row counter
-transpose_next_row:
-    mov     eax, dword [esi]
     
-    mov dword [edi],eax
-    call print_eax
-    add edi, 4
+
+transpose:  ; Transpose A into B
+    mov esi, A
+    mov edi, B
+    mov ecx, 0  ; row counter
+    
+transpose_next_row:
+    mov     ebx,0   ; Column counter.
+
+transpose_next_col:
+    mov edx, dword [esi]  ; Store entry in A in EDX
+    mov eax, edx
+    
+    ; Compute offset for entry into B
+    mov eax, HEIGHT
+    mul bl
+    add eax, ecx
+    mul [four]  ; Formula for new offset: 4(row + HEIGHT * col)
+
+    mov edi, B
+    add edi, eax
+    mov eax, edx
+    mov dword [edi], edx
+    
+    ;call print_eax
     
     add     esi,4
+    inc     ebx
+    cmp     ebx,WIDTH
+    jnz transpose_next_col
     
     inc     ecx
     cmp     ecx,HEIGHT
-    jnz     transpose_next_row    ; Loop until all rows completed
+    jnz transpose_next_row
     
-    inc     ebx
-    cmp     ebx,WIDTH
-    jnz     transpose_next_column ; Loop until all columns completed in row
+
+print_B_init:
+    mov eax, WIDTH
+    mov ebx, HEIGHT
+    mul ebx
+    mov ecx, eax    ; loop counter
     
+    mov esi, B
+
+print_B_loop:
+    mov eax, [esi]
+    call print_eax
+    add esi, 4
+    loop print_B_loop
+
 
     ; Exit the process:
     push    0
