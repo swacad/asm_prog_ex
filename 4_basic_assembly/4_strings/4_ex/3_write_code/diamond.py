38c8a8368a878b57b218359381116cd84d4b5aff
swacad <swacademics@gmail.com>
2018-06-15 16:30:38 -0700

    Change total_stars to num_lines

--- 4_basic_assembly/4_strings/4_ex/3_write_code/diamond.py
+++ 4_basic_assembly/4_strings/4_ex/3_write_code/diamond.py
@@ -1,25 +1,25 @@
 def diamond(n):
-    total_stars = 2 * n + 1
-    print('total_stars = ' + str(total_stars))
+    num_lines = 2 * n + 1
+    print('total_stars = ' + str(num_lines))
 
-    first_half = total_stars // 2 + 1
+    first_half = num_lines // 2 + 1
 
     for i in range(first_half):
-        star_str = total_stars * '-'
+        star_str = num_lines * '-'
         star_str = list(star_str)
 
         num_stars = 2 * i + 1
-        star_idx = total_stars // 2 - i
+        star_idx = num_lines // 2 - i
         for j in range(star_idx, star_idx + num_stars):
             star_str[j] = '*'
         print(''.join(star_str))
 
     for i in range(first_half - 2, -1, -1):
-        star_str = total_stars * '-'
+        star_str = num_lines * '-'
         star_str = list(star_str)
 
         num_stars = 2 * i + 1
-        star_idx = total_stars // 2 - i
+        star_idx = num_lines // 2 - i
         for j in range(star_idx, star_idx + num_stars):
             star_str[j] = '*'
         print(''.join(star_str))
